caea2109960fccf83a4b3190b97db3bc9e677439
Victor Hugo <victor_bmaximo@hotmail.com>
2018-08-02 09:12:12 -0300

    Add Node and Gulp files

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+/*
+ * Variables
+ */
+// Sass Source
+var scssFiles = './src/scss/icons.scss';
+
+// CSS destination
+var cssDest = './css';
+
+// Options for development
+var sassDevOptions = {
+  outputStyle: 'expanded'
+}
+
+// Options for production
+var sassProdOptions = {
+  outputStyle: 'compressed'
+}
+
+/*
+ * Tasks
+ */
+// Task 'sassdev' - Run with command 'gulp sassdev'
+gulp.task('sassdev', function() {
+  return gulp.src(scssFiles)
+    .pipe(sass(sassDevOptions).on('error', sass.logError))
+    .pipe(gulp.dest(cssDest));
+});
+
+// Task 'sassprod' - Run with command 'gulp sassprod'
+gulp.task('sassprod', function() {
+  return gulp.src(scssFiles)
+    .pipe(sass(sassProdOptions).on('error', sass.logError))
+    .pipe(rename('icons.min.css'))
+    .pipe(gulp.dest(cssDest));
+});
+
+// Task 'watch' - Run with command 'gulp watch'
+gulp.task('watch', function() {
+  gulp.watch(scssFiles, ['sassdev', 'sassprod']);
+});
+
+// Default task - Run with command 'gulp'
+gulp.task('default', ['sassdev', 'sassprod', 'watch']);
